869a18ce43763b18b47c76ccf1e2143fbb2db7d0
Xinxin Li <44799727+iamstarlee@users.noreply.github.com>
2025-09-08 10:47:02 +0800

    Add files via upload

---
+++ nets/yolo.py
@@ -0,0 +1,331 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from nets.backbone import C2f, Conv # Backbone
+from nets.backbone_and_vae import Backbone
+from nets.yolo_training import weights_init
+from utils.utils_bbox import make_anchors
+
+from nets.neck import BateVAE
+from typing import List, Callable, Union, Any, TypeVar, Tuple
+Tensor = TypeVar('torch.tensor')
+
+def fuse_conv_and_bn(conv, bn):
+    # 混合Conv2d + BatchNorm2d 减少计算量
+    # Fuse Conv2d() and BatchNorm2d() layers https://tehnokv.com/posts/fusing-batchnorm-and-conv/
+    fusedconv = nn.Conv2d(conv.in_channels,
+                          conv.out_channels,
+                          kernel_size=conv.kernel_size,
+                          stride=conv.stride,
+                          padding=conv.padding,
+                          dilation=conv.dilation,
+                          groups=conv.groups,
+                          bias=True).requires_grad_(False).to(conv.weight.device)
+
+    # 准备kernel
+    w_conv = conv.weight.clone().view(conv.out_channels, -1)
+    w_bn = torch.diag(bn.weight.div(torch.sqrt(bn.eps + bn.running_var)))
+    fusedconv.weight.copy_(torch.mm(w_bn, w_conv).view(fusedconv.weight.shape))
+
+    # 准备bias
+    b_conv = torch.zeros(conv.weight.size(0), device=conv.weight.device) if conv.bias is None else conv.bias
+    b_bn = bn.bias - bn.weight.mul(bn.running_mean).div(torch.sqrt(bn.running_var + bn.eps))
+    fusedconv.bias.copy_(torch.mm(w_bn, b_conv.reshape(-1, 1)).reshape(-1) + b_bn)
+
+    return fusedconv
+
+class DFL(nn.Module):
+    # DFL模块
+    # Distribution Focal Loss (DFL) proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
+    def __init__(self, c1=16):
+        super().__init__()
+        self.conv   = nn.Conv2d(c1, 1, 1, bias=False).requires_grad_(False)
+        x           = torch.arange(c1, dtype=torch.float)
+        self.conv.weight.data[:] = nn.Parameter(x.view(1, c1, 1, 1))
+        self.c1     = c1
+
+    def forward(self, x):
+        # bs, self.reg_max * 4, 8400
+        b, c, a = x.shape
+        # bs, 4, self.reg_max, 8400 => bs, self.reg_max, 4, 8400 => b, 4, 8400
+        # 以softmax的方式，对0~16的数字计算百分比，获得最终数字。
+        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
+        # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+        
+#---------------------------------------------------#
+#   yolo_body
+#---------------------------------------------------#
+class YoloBody(nn.Module):
+    def __init__(self, input_shape, num_classes, phi, pretrained=False):
+        super(YoloBody, self).__init__()
+        depth_dict          = {'n' : 0.33, 's' : 0.33, 'm' : 0.67, 'l' : 1.00, 'x' : 1.00,}
+        width_dict          = {'n' : 0.25, 's' : 0.50, 'm' : 0.75, 'l' : 1.00, 'x' : 1.25,}
+        deep_width_dict     = {'n' : 1.00, 's' : 1.00, 'm' : 0.75, 'l' : 0.50, 'x' : 0.50,}
+        dep_mul, wid_mul, deep_mul = depth_dict[phi], width_dict[phi], deep_width_dict[phi]
+
+        base_channels       = int(wid_mul * 64)  # 64
+        base_depth          = max(round(dep_mul * 3), 1)  # 3
+        #-----------------------------------------------#
+        #   输入图片是3, 640, 640
+        #-----------------------------------------------#
+
+        #---------------------------------------------------#   
+        #   生成主干模型
+        #   获得三个有效特征层，他们的shape分别是：
+        #   256, 80, 80
+        #   512, 40, 40
+        #   1024 * deep_mul, 20, 20
+        #---------------------------------------------------#
+        self.backbone = Backbone(base_channels, base_depth, deep_mul, phi, pretrained=pretrained)
+
+        #------------------------加强特征提取网络------------------------# 
+        self.upsample   = nn.Upsample(scale_factor=2, mode="nearest")
+
+        # 1024 * deep_mul + 512, 40, 40 => 512, 40, 40
+        self.conv3_for_upsample1    = C2f(int(base_channels * 16 * deep_mul) + base_channels * 8, base_channels * 8, base_depth, shortcut=False)
+        # 768, 80, 80 => 256, 80, 80
+        self.conv3_for_upsample2    = C2f(base_channels * 8 + base_channels * 4, base_channels * 4, base_depth, shortcut=False)
+        
+        # 256, 80, 80 => 256, 40, 40
+        self.down_sample1           = Conv(base_channels * 4, base_channels * 4, 3, 2)
+        # 512 + 256, 40, 40 => 512, 40, 40
+        self.conv3_for_downsample1  = C2f(base_channels * 8 + base_channels * 4, base_channels * 8, base_depth, shortcut=False)
+
+        # 512, 40, 40 => 512, 20, 20
+        self.down_sample2           = Conv(base_channels * 8, base_channels * 8, 3, 2)
+        # 1024 * deep_mul + 512, 20, 20 =>  1024 * deep_mul, 20, 20
+        self.conv3_for_downsample2  = C2f(int(base_channels * 16 * deep_mul) + base_channels * 8, int(base_channels * 16 * deep_mul), base_depth, shortcut=False)
+        #------------------------加强特征提取网络------------------------# 
+        
+        ch              = [base_channels * 4, base_channels * 8, int(base_channels * 16 * deep_mul)]
+        self.shape      = None
+        self.nl         = len(ch)
+        # self.stride     = torch.zeros(self.nl)
+        self.stride     = torch.tensor([256 / x.shape[-2] for x in self.backbone.forward(torch.zeros(1, 3, 256, 256))])  # forward
+        self.reg_max    = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
+        self.no         = num_classes + self.reg_max * 4  # number of outputs per anchor
+        self.num_classes = num_classes
+        
+        c2, c3   = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], num_classes)  # channels
+        self.cv2 = nn.ModuleList(nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
+        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, num_classes, 1)) for x in ch)
+        if not pretrained:
+            weights_init(self)
+        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
+
+        self.training_head = False
+        self.is_vae = False  
+
+    #     #------------------------变分自编码器网络------------------------#
+        
+    #     self.latent_dim = 12800
+    #     self.beta = 4
+    #     self.gamma = 1000.
+    #     self.loss_type = 'B'
+    #     self.C_max = torch.Tensor([25])
+    #     self.C_stop_iter = 1e5
+
+    #     self.hidden_dims = [32, 64, 128, 256, 512] 
+        
+    #     self.in_channels = 1280
+    #     self.outchannels = 1280
+
+    #     modules = []
+    #     hidden_dims = [32, 64, 128, 256, 512]
+
+    #     # Encoder
+    #     in_channels = self.in_channels
+    #     for h_dim in hidden_dims:
+    #         modules.append(
+    #             nn.Sequential(
+    #                 nn.Conv2d(in_channels, out_channels=h_dim,
+    #                           kernel_size= 3, stride= 2, padding  = 1),
+    #                 nn.BatchNorm2d(h_dim),
+    #                 nn.LeakyReLU())
+    #         )
+    #         in_channels = h_dim
+
+    #     self.encoder = nn.Sequential(*modules)
+    #     self.fc_mu = nn.Linear(hidden_dims[-1]*4, self.latent_dim)
+    #     self.fc_var = nn.Linear(hidden_dims[-1]*4, self.latent_dim)
+
+    #     # Decoder
+    #     modules = []
+
+    #     self.decoder_input = nn.Linear(self.latent_dim, hidden_dims[-1] * 4)
+
+    #     hidden_dims.reverse()
+
+    #     for i in range(len(hidden_dims) - 1):
+    #         modules.append(
+    #             nn.Sequential(
+    #                 nn.ConvTranspose2d(hidden_dims[i],
+    #                                    hidden_dims[i + 1],
+    #                                    kernel_size=3,
+    #                                    stride = 2,
+    #                                    padding=1,
+    #                                    output_padding=1),
+    #                 nn.BatchNorm2d(hidden_dims[i + 1]),
+    #                 nn.LeakyReLU())
+    #         )
+
+    #     self.decoder = nn.Sequential(*modules)
+    #     self.size = (40,40)
+    #     self.view = (-1, 512, 2, 2)
+
+
+    #     self.final_layer = nn.Sequential(
+    #         nn.ConvTranspose2d(hidden_dims[-1], hidden_dims[-1], kernel_size=3, stride=2, padding=1, output_padding=1),
+    #         nn.BatchNorm2d(hidden_dims[-1]),
+    #         nn.LeakyReLU(),
+    #         nn.Conv2d(hidden_dims[-1], hidden_dims[-1], kernel_size=3, padding=1),
+    #         nn.LeakyReLU(),
+    #         nn.Conv2d(hidden_dims[-1], out_channels=self.outchannels, kernel_size=3, padding=1), # 512可用
+    #         # nn.Conv2d(hidden_dims[-1], out_channels=256, kernel_size=3, padding=1), # 256可用
+    #         nn.Tanh(),
+    #         nn.AdaptiveAvgPool2d(self.size)  # 自适应调整到 [512, 20, 20]
+    #         # nn.AdaptiveAvgPool2d((40, 40))
+    #         # nn.AdaptiveAvgPool2d((80, 80))
+    #     )
+
+    # def encode(self, input:Tensor) -> List[Tensor]:
+    #     result = self.encoder(input)
+    #     result = torch.flatten(result, start_dim=1)
+    #     mu = self.fc_mu(result)
+    #     log_var = self.fc_var(result)
+    #     return [mu, log_var]
+    
+    # def decode(self, z: Tensor) -> Tensor:
+    #     result = self.decoder_input(z)
+    #     result = result.view(self.view) # 20,40可用
+    #     # result = result.view(-1, 576, 2, 2) # 80可用
+    #     result = self.decoder(result)
+    #     result = self.final_layer(result)
+    #     return result
+    
+    # def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
+    #     std = torch.exp(0.5 * logvar)
+    #     eps = torch.randn_like(std)
+    #     return eps * std + mu
+
+    def fuse(self):
+        print('Fusing layers... ')
+        for m in self.modules():
+            if type(m) is Conv and hasattr(m, 'bn'):
+                m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
+                delattr(m, 'bn')  # remove batchnorm
+                m.forward = m.forward_fuse  # update forward
+        return self
+    
+    def forward(self, x):
+        # backbone
+        if self.training_head == True and self.is_vae is False:
+            feat1, feat2, feat3 = self.backbone.forward(x)
+            # Encode features
+            # 处理第一和第三个维度的特征
+            feat1 = F.interpolate(feat1, size=(40, 40), mode='bilinear', align_corners=False) # 1*256*80*80 => 1*256*40*40
+            feat3 = F.interpolate(feat3, size=(40, 40), mode='bilinear', align_corners=False) # 1*512*20*20 => 1*512*40*40
+            feat_three = torch.cat([feat1, feat2, feat3], 1) # 1*1280*40*40
+            
+            # 融合版本
+            mu, log_var = self.encode(feat_three)
+            z = self.reparameterize(mu, log_var)
+            return z
+        if self.training_head is False and self.is_vae is False:
+            feat1, feat2, feat3 = self.backbone.forward_ori(x)
+            # print(f"feat1 shape: {feat1.shape}, feat2 shape: {feat2.shape}, feat3 shape: {feat3.shape}")
+            
+            # # Encode features
+            # # 处理第一和第三个维度的特征
+            # feat1 = F.interpolate(feat1, size=(40, 40), mode='bilinear', align_corners=False) # 1*256*80*80 => 1*256*40*40
+            # feat3 = F.interpolate(feat3, size=(40, 40), mode='bilinear', align_corners=False) # 1*512*20*20 => 1*512*40*40
+            # feat_three = torch.cat([feat1, feat2, feat3], 1) # 1*1280*40*40
+            
+            # # 融合版本
+            # mu, log_var = self.encode(feat_three)
+            # z = self.reparameterize(mu, log_var)
+            # feat_three = self.decode(z)
+            
+            # # 还原第一和第三个维度的特征
+            # feat1, feat2, feat3 = feat_three.split([256, 512, 512], 1)
+            # feat1 = F.interpolate(feat1, size=(80, 80), mode='bilinear', align_corners=False) # 1*256*40*40 => 1*256*80*80
+            # feat3 = F.interpolate(feat3, size=(20, 20), mode='bilinear', align_corners=False) # 1*512*40*40 => 1*512*20*20
+        
+        if self.training_head is False and self.is_vae is True:
+            feat1, feat2, feat3 = self.backbone.forward_ori(x)
+            print(f"feat1 shape: {feat1.shape}, feat2 shape: {feat2.shape}, feat3 shape: {feat3.shape}")
+            
+            # 处理第一和第三个维度的特征
+            feat1 = F.interpolate(feat1, size=(40, 40), mode='bilinear', align_corners=False) # 1*256*80*80 => 1*256*40*40
+            feat3 = F.interpolate(feat3, size=(40, 40), mode='bilinear', align_corners=False) # 1*512*20*20 => 1*512*40*40
+            feat_three = torch.cat([feat1, feat2, feat3], 1) # 1*1280*40*40
+            # print(f"feat_three shape: {feat_three.shape}")
+            # 融合版本
+            mu, log_var = self.encode(feat_three)
+            z = self.reparameterize(mu, log_var)
+            # return z
+
+
+
+            feat_three = self.decode(z)
+
+            # 还原第一和第三个维度的特征
+            feat1, feat2, feat3 = feat_three.split([256, 512, 512], 1)
+            # feat1, feat2, feat3 = feat_three.split([320, 640, 640], 1)
+            feat1 = F.interpolate(feat1, size=(80, 80), mode='bilinear', align_corners=False) # 1*256*40*40 => 1*256*80*80
+            feat3 = F.interpolate(feat3, size=(20, 20), mode='bilinear', align_corners=False) # 1*512*40*40 => 1*512*20*20
+        
+
+        #------------------------加强特征提取网络------------------------# 
+        # 1024 * deep_mul, 20, 20 => 1024 * deep_mul, 40, 40
+        P5_upsample = self.upsample(feat3)
+        # 1024 * deep_mul, 40, 40 cat 512, 40, 40 => 1024 * deep_mul + 512, 40, 40
+        P4          = torch.cat([P5_upsample, feat2], 1)
+        # 1024 * deep_mul + 512, 40, 40 => 512, 40, 40
+        P4          = self.conv3_for_upsample1(P4)
+
+        # 512, 40, 40 => 512, 80, 80
+        P4_upsample = self.upsample(P4)
+        # 512, 80, 80 cat 256, 80, 80 => 768, 80, 80
+        P3          = torch.cat([P4_upsample, feat1], 1)
+        # 768, 80, 80 => 256, 80, 80
+        P3          = self.conv3_for_upsample2(P3)
+
+        # 256, 80, 80 => 256, 40, 40
+        P3_downsample = self.down_sample1(P3)
+        # 512, 40, 40 cat 256, 40, 40 => 768, 40, 40
+        P4 = torch.cat([P3_downsample, P4], 1)
+        # 768, 40, 40 => 512, 40, 40
+        P4 = self.conv3_for_downsample1(P4)
+
+        # 512, 40, 40 => 512, 20, 20
+        P4_downsample = self.down_sample2(P4)
+        # 512, 20, 20 cat 1024 * deep_mul, 20, 20 => 1024 * deep_mul + 512, 20, 20
+        P5 = torch.cat([P4_downsample, feat3], 1)
+        # 1024 * deep_mul + 512, 20, 20 => 1024 * deep_mul, 20, 20
+        P5 = self.conv3_for_downsample2(P5)
+        #------------------------加强特征提取网络------------------------# 
+        # P3 256, 80, 80
+        # P4 512, 40, 40
+        # P5 1024 * deep_mul, 20, 20
+        shape = P3.shape  # BCHW
+        
+        # P3 256, 80, 80 => num_classes + self.reg_max * 4, 80, 80
+        # P4 512, 40, 40 => num_classes + self.reg_max * 4, 40, 40
+        # P5 1024 * deep_mul, 20, 20 => num_classes + self.reg_max * 4, 20, 20
+        x = [P3, P4, P5]
+        for i in range(self.nl):
+            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+
+        if self.shape != shape:
+            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
+            self.shape = shape
+        
+        # num_classes + self.reg_max * 4 , 8400 =>  cls num_classes, 8400; 
+        #                                           box self.reg_max * 4, 8400
+        box, cls        = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2).split((self.reg_max * 4, self.num_classes), 1)
+        # origin_cls      = [xi.split((self.reg_max * 4, self.num_classes), 1)[1] for xi in x]
+        dbox            = self.dfl(box)
+        return dbox, cls, x, self.anchors.to(dbox.device), self.strides.to(dbox.device)
